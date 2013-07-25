67683b5d03d599b4ec3112e99765c81864b209b0
wujian016 <wujian016@163.com>
2013-07-26 06:22:35 +0800

    test 5

--- MvcApplication1/Models/Movie.cs
+++ MvcApplication1/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace MvcApplication1.Models
@@ -6,10 +7,14 @@ namespace MvcApplication1.Models
     public class Movie
     {
         public int ID { get; set; }
+        [Required]
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Range(1, 100)]
         public decimal Price { get; set; }
+        [StringLength(5)]
+        public string Rating { get; set; }
     }
 
     public class MovieDBContext : DbContext
